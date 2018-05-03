42ec0a543f08429280e2bc202ef5b395314cac1e
Lars Henning Klein <lars.klein@rwth-aachen.de>
2018-05-03 15:17:06 +0200

    adding a file to collect small helper functions for visualization

---
+++ visualization_helpers.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def show_img(img):
+
+    assert len(img.shape) in [2, 3], "x, y, [color]"
+    if len(img.shape) == 3:
+        assert img.shape[2]== 3, "rgb"
+
+    plt.imshow(img)
+    plt.show()
