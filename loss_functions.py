58e78fe260aefae9f25e68d2254114ce98e80df5
Lars Henning Klein <lars.klein@rwth-aachen.de>
2018-05-07 13:59:12 +0200

    choosing a specific delta, making square and linear part more clear.

--- loss_functions.py
+++ loss_functions.py
@@ -1,13 +1,15 @@
-import numpy as np
 import tensorflow as tf
 
 
 def huber_loss(y_true, y_pred):
     # huber loss
     delta = 1
-    diff = y_true - y_pred
-    mask = tf.abs(diff) < delta
+    error = tf.abs(y_true - y_pred)
+    mask = tf.abs(error) < delta
     mask = tf.cast(mask, tf.float32)
 
-    huber = 1 / 2 * diff ** 2 * mask + delta * (tf.abs(diff) - 1 / 2 * delta) * (1 - mask)
+    square = 1 / 2 * error ** 2 * mask
+    linear = 1 / 2 * error * (1 - mask)
+
+    huber = square + linear
     return huber
