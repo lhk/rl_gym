cf95962737197eb87b4a2cf32ecd622275e81265
Lars Henning Klein <lars.klein@rwth-aachen.de>
2018-05-08 00:17:30 +0200

    cleaning up huber loss, avoiding tf.cast

--- loss_functions.py
+++ loss_functions.py
@@ -3,13 +3,11 @@ import tensorflow as tf
 
 def huber_loss(y_true, y_pred):
     # huber loss
-    delta = 1
     error = tf.abs(y_true - y_pred)
-    mask = tf.abs(error) < delta
-    mask = tf.cast(mask, tf.float32)
+    mask = tf.abs(error) < 1
 
-    square = 1 / 2 * error ** 2 * mask
-    linear = 1 / 2 * error * (1 - mask)
+    square = 1 / 2 * error**2
+    linear = 1 / 2 * error
 
-    huber = square + linear
+    huber = tf.select(mask, square, linear)
     return huber
