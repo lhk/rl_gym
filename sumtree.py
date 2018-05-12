d5d2a973e8b8a71ad178699861ecaa16cc7f5e97
lhk <lars.klein@rwth-aachen.de>
2018-05-13 00:06:51 +0200

    wip: debugging sampling.

--- sumtree.py
+++ sumtree.py
@@ -1,3 +1,5 @@
+# using this structure for the binary tree: http://www.cs.northwestern.edu/academics/courses/311/html/tree-notes.html#heaps
+
 import numpy as np
 
 class SumTree:
@@ -17,20 +19,47 @@ class SumTree:
     def push(self, idx, val):
         arr_idx = self._offset + idx
 
-        delta = val - self.data[arr_idx]
-        self.data[arr_idx] = val
+        delta = val - self._data[arr_idx]
+        self._data[arr_idx] = val
 
         parent_idx = (arr_idx - 1) // 2
         arr_idx = parent_idx
-        self.data[arr_idx] += delta
+        self._data[arr_idx] += delta
 
         while (parent_idx != 0):
             parent_idx = (arr_idx - 1) // 2
             arr_idx = parent_idx
-            self.data[arr_idx] += delta
+            self._data[arr_idx] += delta
 
     def sample(self, n):
-        rand = np.random.rand(n)
-        rand*= self._data[0]
+        samples = []
+        for i in range(n):
+            rand = np.random.rand()
+            rand *= self._data[0]
 
-        print("stop here")
+            idx = 0
+            while(idx < self._offset):
+                left_idx = 2*idx + 1
+                right_idx = 2*idx + 2
+
+                left_val = self._data[left_idx]
+
+                if left_val >= rand:
+                    idx = left_idx
+                else:
+                    idx = right_idx
+                    rand-= left_val
+
+            samples.append(idx-self._offset)
+
+        return np.array(samples)
+
+tree = SumTree(4)
+tree.push(0, 1)
+tree.push(1, 2)
+tree.push(2, 3)
+tree.push(3, 4)
+
+res = tree.sample(500)
+
+print("stop mark")
