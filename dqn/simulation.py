504038301f634d4c27099b0a32de5c58b8a050ef
lhk <lars.klein@rwth-aachen.de>
2018-05-16 15:29:47 +0200

    fixing tf import problems, adding progress bar

--- dqn/simulation.py
+++ dqn/simulation.py
@@ -1,15 +1,18 @@
 import numpy as np
+import tensorflow as tf
 
 from dqn.agent import Agent
 from dqn.memory import Memory
 from dqn.brain import Brain
 import dqn.params as params
 
+from tqdm import tqdm
+
 agent = Agent()
 memory = Memory()
 brain = Brain()
 
-for interaction in range(params.TOTAL_INTERACTIONS):
+for interaction in tqdm(range(params.TOTAL_INTERACTIONS), smoothing=1):
 
     # use the brain to determine the best action for this state
     q_values = brain.predict_q(agent.state)
@@ -18,6 +21,10 @@ for interaction in range(params.TOTAL_INTERACTIONS):
     # let the agent interact with the environment and memorize the result
     from_state, to_state, action, reward, done = agent.act(best_action)
     memory.push(from_state, to_state, action, reward, done)
+
+    # fill the memory before training
+    if interaction < params.REPLAY_START_SIZE:
+        continue
 
     # train the network every N steps
     if interaction % params.TRAIN_SKIPS != 0:
