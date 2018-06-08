6c002062446004255f429c342aca6fcd9c3b3c8f
lhk <lars.klein@rwth-aachen.de>
2018-06-09 00:59:32 +0200

    reformatting

--- np_draw/main.py
+++ np_draw/main.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 from np_draw.sprite import Sprite
 
@@ -7,8 +7,8 @@ cross_img = np.zeros((30, 30))
 cross_img[13:16, :] = 1
 cross_img[:, 13:16] = 1
 
-cross_sprite = Sprite(cross_img, cross_img>0, np.array([30, 16]), 75)
-cross_sprite2 = Sprite(cross_img, cross_img>0, np.array([25, 35]), 0)
+cross_sprite = Sprite(cross_img, cross_img > 0, np.array([30, 16]), 75)
+cross_sprite2 = Sprite(cross_img, cross_img > 0, np.array([25, 35]), 0)
 
 canvas = np.zeros((100, 100))
 cross_sprite.render(canvas)
--- np_draw/sprite.py
+++ np_draw/sprite.py
@@ -1,15 +1,14 @@
 import numpy as np
 from skimage.transform import rotate
-import matplotlib.pyplot as plt
 
 
 def render(img, mask, upperleft, canvas):
-    assert np.all(upperleft>=0), "image must be withing canvas"
+    assert np.all(upperleft >= 0), "image must be withing canvas"
     assert np.all(upperleft + img.shape <= canvas.shape), "image must be within canvas"
 
     shape = img.shape[:2]
     canvas_slice = canvas[upperleft[0]:upperleft[0] + shape[0],
-            upperleft[1]:upperleft[1] + shape[1]]
+                   upperleft[1]:upperleft[1] + shape[1]]
     area = canvas_slice.shape[:2]
 
     mask_slice = mask[-area[0]:, -area[1]:]
@@ -17,6 +16,7 @@ def render(img, mask, upperleft, canvas):
     canvas_slice[mask_slice] = img_slice[mask_slice]
 
     return canvas
+
 
 class Sprite():
     def __init__(self, img, mask, pos, rot):
@@ -28,11 +28,11 @@ class Sprite():
 
     def set_rotation(self, new_rot):
         self.rot = new_rot
-        self.img_rotated=rotate(self.img, self.rot, resize=True, order=1)
+        self.img_rotated = rotate(self.img, self.rot, resize=True, order=1)
         self.mask_rotated = rotate(self.mask, self.rot, resize=True, order=1).astype(np.bool)
 
         self.size = np.array(self.img_rotated.shape[:2])
-        self.upperleft = self.pos - self.size/2
+        self.upperleft = self.pos - self.size / 2
 
         self.size_int = self.size.astype(np.int)
         self.upperleft_int = self.upperleft.astype(np.int)
@@ -42,7 +42,7 @@ class Sprite():
     def render(self, canvas):
         # cut away parts of the image that are outside of the canvas
         # after that, upperleft can be clipped
-        cutoff = np.abs(self.upperleft_int) * (self.upperleft_int<0)
+        cutoff = np.abs(self.upperleft_int) * (self.upperleft_int < 0)
         img_visible = self.img_rotated[cutoff[0]:, cutoff[1]:]
         img_visible = img_visible[:canvas.shape[0], :canvas.shape[1]]
 
@@ -65,10 +65,10 @@ class Sprite():
         # render the two images and compare pixels
         offset = (dist_vec < 0).astype(np.int) * -dist_vec
         other_pos = offset.astype(np.int)
-        own_pos = (dist_vec+offset).astype(np.int)
+        own_pos = (dist_vec + offset).astype(np.int)
 
-        max_x = max(own_pos[0]+self.size[0], other_pos[0]+other.size[0])
-        max_y = max(own_pos[1]+self.size[1], other_pos[1]+other.size[1])
+        max_x = max(own_pos[0] + self.size[0], other_pos[0] + other.size[0])
+        max_y = max(own_pos[1] + self.size[1], other_pos[1] + other.size[1])
 
         own_canvas = np.zeros((max_x, max_y))
         other_canvas = np.zeros((max_x, max_y))
@@ -76,5 +76,5 @@ class Sprite():
         render(self.img_rotated, self.mask_rotated, own_pos, own_canvas)
         render(other.img_rotated, other.mask_rotated, other_pos, other_canvas)
 
-        intersection = (own_canvas>0) * (other_canvas>0)
+        intersection = (own_canvas > 0) * (other_canvas > 0)
         return np.any(intersection)
