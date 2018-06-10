10ecdb02221f62a1a4965fdc808d746721e68b8e
lhk <lars.klein@rwth-aachen.de>
2018-06-10 13:12:31 +0200

    switching to er, current training setup

--- dqn/dqn_train.py
+++ dqn/dqn_train.py
@@ -6,9 +6,9 @@ print(tf.GRAPH_DEF_VERSION)  # and if I don't use it, autoformatting gets rid of
 
 import dqn.params as params
 import environments.obstacle_car.params as env_params
-from dqn.agent import PER_Agent
+from dqn.agent import ER_Agent
 from dqn.brain import DQN_Brain
-from dqn.memory import Priority_Memory
+from dqn.memory import Equal_Memory
 from environments.obstacle_car.environment import Environment_Graphical as Environment
 
 from util.loss_functions import huber_loss
@@ -22,10 +22,10 @@ if vis:
     window = pygame.display.set_mode(env_params.screen_size)
     pygame.display.set_caption("observations")
 
-memory = Priority_Memory()
+memory = Equal_Memory()
 brain = DQN_Brain(memory, loss=huber_loss)
 env = Environment()
-agent = PER_Agent(memory, brain, env)
+agent = ER_Agent(memory, brain, env)
 
 for interaction in tqdm(range(params.TOTAL_INTERACTIONS), smoothing=1):
 
