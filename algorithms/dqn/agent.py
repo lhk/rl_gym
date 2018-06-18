40977bf733f2b128368c40d83176a2a46d29fd14
lhk <lars.klein@rwth-aachen.de>
2018-06-18 22:26:16 +0200

    wip: introducing observation / state distinction to agent

--- algorithms/dqn/agent.py
+++ algorithms/dqn/agent.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 import algorithms.dqn.params as params
-from algorithms.dqn.preprocessing import preprocess_frame
 
 
 class Agent:
@@ -20,59 +19,25 @@ class Agent:
         # current probability of random action
         self.exploration = params.INITIAL_EXPLORATION
 
-        # current state
-        # can be different from the env state
-        # frame stacking, resizing, etc
-        self.state = self.get_starting_state()
+        self.reset()
+
+
+    def reset(self):
+        self.observation = self.env.reset()
+
+        # internally, the brain uses a model, and that can have a state, rnns for example
+        self.state = self.brain.get_initial_state()
+        self.observation = self.brain.preprocess(self.observation, self.state)
+
+        if params.FRAME_STACK:
+            self.observation = np.stack([self.observation]*params.FRAME_STACK, axis=-1)
+
         self.total_reward = 0
 
         # don't repeat actions too often
         self.last_action = None
         self.repeat_action_counter = 0
 
-    def interact(self, action):
-        """
-        applies the action to the environment.
-        this takes care of preprocessing observations
-        and updating the internal state of the agent
-        :param action: action to apply
-        :return: new_state, reward, done
-        """
-        observation, reward, done = self.env.step(action)
-
-        new_frame = preprocess_frame(observation)
-
-        new_state = np.empty_like(self.state)
-        new_state[:, :, :-1] = self.state[:, :, 1:]
-        new_state[:, :, -1] = new_frame
-
-        return new_state, reward, done
-
-    def render_frame(self):
-        pass
-
-    def render_episode(self):
-        pass
-
-    def get_starting_state(self):
-        self.state = np.zeros(params.INPUT_SHAPE, dtype=np.uint8)
-        self.env.reset()
-        frame = self.env.render()
-        self.state[:, :, -1] = preprocess_frame(frame)
-
-        action = 0
-
-        # we repeat the action 4 times, since our initial state needs 4 stacked frames
-        for i in range(params.FRAME_STACK):
-            state, _, _ = self.interact(action)
-
-        return state
-
-
-class ER_Agent(Agent):
-    def __init__(self, memory, brain, environment):
-        Agent.__init__(self, memory, brain, environment)
-
     def act(self):
         """
         chooses an action and applies it to the environment.
@@ -80,13 +45,30 @@ class ER_Agent(Agent):
         :return:
         """
 
-        # exploitation vs exploration
-        if np.random.rand() < self.exploration:
-            action = np.random.choice(params.NUM_ACTIONS)
-        else:
+        # if the brain is stateful, then the state can change due to this observation
+        # we have to show the observation to the brain in any case, to get the new state
+        # if the brain is not stateful, we have to get predictions for this observation only if we decide to exploit
+        # if the brain is not stateful and we decide to explore, there is no need for predicting the q values
+        # this is a potentially huge speedup
+        if self.brain.stateful:
             # use the brain to determine the best action for this state
-            current_q = self.brain.predict_q(self.state)[0]
+            current_q, to_state = self.brain.predict_q(self.observation, self.state)[0]
             action = current_q.argmax()
+
+            # maybe overwrite the action
+            if np.random.rand() < self.exploration:
+                action = np.random.choice(params.NUM_ACTIONS)
+        else:
+            # exploration vs exploitation
+            if np.random.rand() < self.exploration:
+                action = np.random.choice(params.NUM_ACTIONS)
+            else:
+                # use the brain to determine the best action for this state
+                current_q, to_state = self.brain.predict_q(self.observation, self.state)[0]
+                action = current_q.argmax()
+
+                assert to_state is None
+
 
         # anneal exploration
         if self.exploration > params.FINAL_EXPLORATION:
@@ -105,76 +87,28 @@ class ER_Agent(Agent):
             self.repeat_action_counter = 0
 
         # interact with the environment
-        new_state, reward, done = self.interact(action)
+        new_observation, reward, done, _ = self.env.step(action)
         reward *= params.REWARD_SCALE
         self.total_reward += reward
 
-        from_state = self.state
-        to_state = new_state
+        # preprocess the new observation
+        from_observation = self.observation.copy()
+        from_state = self.state.copy()
+
+        new_observation, to_state = self.brain.preprocess(new_observation, self.state)
+
+        if params.FRAME_STACK:
+            to_observation = np.zeros_like(self.observation)
+            to_observation[:, :, :-1] = self.observation[:, :, 1:]
+            to_observation[:, :, -1] = new_observation
+        else:
+            to_observation = new_observation
 
         if not done:
-            self.state = new_state
+            self.observation = to_observation
+            self.state = to_state
         else:
-            self.state = self.get_starting_state()
-            to_state[:] = 0
-
-            print(self.total_reward)
-            self.total_reward = 0
-
-        # the value that should have been predicted
-        q_target = self.brain.get_targets(to_state, reward, done)
-
-        self.memory.push(from_state, to_state, action, q_target, done)
-
-
-class PER_Agent(Agent):
-    def __init__(self, memory, brain, environment):
-        Agent.__init__(self, memory, brain, environment)
-
-    def act(self):
-        """
-        chooses an action and applies it to the environment.
-        updates the memory to provide training data for the brain
-        :return:
-        """
-
-        # exploration vs exploitation
-        if np.random.rand() < self.exploration:
-            action = np.random.choice(params.NUM_ACTIONS)
-        else:
-            # use the brain to determine the best action for this state
-            current_q = self.brain.predict_q(self.state)[0]
-            action = current_q.argmax()
-
-        # anneal exploration
-        if self.exploration > params.FINAL_EXPLORATION:
-            self.exploration -= params.EXPLORATION_STEP
-
-        # we only allow a limited amount of repeated actions
-        if action == self.last_action:
-            self.repeat_action_counter += 1
-
-            if self.repeat_action_counter > params.REPEAT_ACTION_MAX:
-                action = self.env.sample_action()
-                self.last_action = action
-                self.repeat_action_counter = 0
-        else:
-            self.last_action = action
-            self.repeat_action_counter = 0
-
-        # interact with the environment
-        new_state, reward, done = self.interact(action)
-        reward *= params.REWARD_SCALE
-        self.total_reward += reward
-
-        from_state = self.state
-        to_state = new_state
-
-        if not done:
-            self.state = new_state
-        else:
-            self.state = self.get_starting_state()
-            to_state[:] = 0
+            self.reset()
 
             print(self.total_reward)
             self.total_reward = 0
@@ -192,4 +126,4 @@ class PER_Agent(Agent):
         # new observations are pushed to the memory with a default priority
         # this means that for most interactions, we don't need to use the brain
         # if the agent is exploring, we don't have to calculate any q values
-        self.memory.push(from_state, to_state, action, reward, done, params.DEFAULT_PRIO)
+        self.memory.push(from_observation, to_observation, from_state, to_state, action, reward, done, params.DEFAULT_PRIO)
