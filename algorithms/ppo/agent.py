fixing bug in gae calculation.
before refactoring ppo

algorithms/ppo/agent.py
@@ -150,24 +150,7 @@ class Agent(threading.Thread):
         # read the length first, before popping anything
         length = len(self.seen_actions)
 
-        # compute gae advantage
-        # the series of rewards seen in the memory
-        # the last reward is replaced with the predicted value of the last state
-        rewards = np.array(self.seen_rewards)
-
-        # delta functions are 1 step TD lambda
-        values = np.zeros((length + 1,))
-        values[:-1] = self.seen_values[:]
-        values[-1] = self.seen_values[-1] * (1 - terminal)
-
-        deltas = rewards + params.GAMMA * values[1:] - values[:-1]
-
-        # gae advantage uses a weighted sum of deltas,
-        # compare (16) in the gae paper
-        discount_factor = params.GAMMA * params.LAMBDA
-        weights = np.geomspace(1, discount_factor ** len(deltas), len(deltas))
-        weighted_series = deltas * weights
-        advantage_gae = weighted_series.sum()
+        advantage_gae = self.compute_gae(np.array(self.seen_rewards), np.array(self.seen_values), terminal)
 
         from_observation = self.seen_observations.pop(0)
         from_state = self.seen_states.pop(0)
@@ -182,3 +165,22 @@ class Agent(threading.Thread):
         self.shared_memory.push(batch)
 
         self.n_step_reward = (self.n_step_reward - first_reward)
+
+    def compute_gae(self, rewards, values, terminal):
+        length = len(rewards)
+
+        # delta functions are 1 step TD lambda
+        padded_values = np.zeros((length + 1,))
+        padded_values[:-1] = values
+        padded_values[-1] = values[-1] * (1 - terminal)
+
+        deltas = rewards + params.GAMMA * padded_values[1:] - padded_values[:-1]
+
+        # gae advantage uses a weighted sum of deltas,
+        # compare (16) in the gae paper
+        discount_factor = params.GAMMA * params.LAMBDA
+        weights = np.geomspace(1, discount_factor ** (len(deltas) - 1), len(deltas))
+        weighted_series = deltas * weights
+        advantage_gae = weighted_series.sum()
+
+        return advantage_gae