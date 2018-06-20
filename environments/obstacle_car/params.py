89608c415aedc9166a7442884bc74692c554b610
lhk <lars.klein@rwth-aachen.de>
2018-06-21 00:19:22 +0200

    current params

--- environments/obstacle_car/params.py
+++ environments/obstacle_car/params.py
@@ -7,15 +7,15 @@ num_obstacles = 2
 distance_rescale = 100 # only used in radial environment
 
 # parameters for mrp
-reward_goal = 2
+reward_goal = 2.
 reward_distance = 0.005
-reward_collision = -2
+reward_collision = -4.
 reward_timestep = -0.1
 timeout = 600
 max_dist = 1.05
 
 # parameters for simulation
-dT = 2
+dT = 1.5
 min_speed = -8
 max_speed = 10
 stop_on_border_collision = True
